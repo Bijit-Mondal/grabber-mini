b3bed7470ae70a2918e6fb24ee955a28c72be933
Bijit-Mondal <68532969+Bijit-Mondal@users.noreply.github.com>
2021-08-22 17:31:36 +0530

    Update youtube_search.js

--- routes/youtube_search.js
+++ routes/youtube_search.js
@@ -69,56 +69,7 @@ router.get('/download/audio/:vid/:name?', (req,res,next)=>{
             next(error);
     }
 });
-   /* try {
-            // response attachment for triggering download instead of stream 
-           res.attachment(`${name}.mp3`);
-            
-            // For Extracting mp3 from video stream of youtube-dl , on the fly(pipe)
-            let ffplay_child = spawn("ffmpeg", [
-                '-i', //input
-                'pipe:0', //stdin
-               '-acodec', //audio codec
-                'libmp3lame',//external encoding library
-                '-f', //format
-                'mp3',//mp3
-                '-'//output to stdout
-            ])
-            .on('error',(err)=>next(err))
-            .on('exit',(code)=>console.log(`Ffmpeg exited with code ${code}`));
-
-            //Catching Errors on stdin 
-            ffplay_child.stdin.on('error',(err)=>next(err));
-
-            //Setting output pipe first so that we dont lose any bits
-            ffplay_child.stdout.pipe(res).on('error',(err)=>next(err));
-
-            // For Downloading video from youtube using video-id
-            const ytdl = spawn('youtube-dl', [
-                '-o',//output
-                '-',//stdout
-                '-a',//input stream
-                '-'//stdin
-            ])
-            .on('error',(err)=>next(err))
-            .on('exit',(code)=>console.log(`Ytdl exited with code ${code}`));
-            
-            // Setting output pipe first so that we dont lose any bits 
-            ytdl.stdout.pipe(ffplay_child.stdin).on('error',(err)=>next(err));
-
-            //Catching errors on stdin
-            ytdl.stdin.on('error',(err)=>next(err));
-
-            // Writing video url to stdin for youtube-dl 
-            ytdl.stdin.write(`http://www.youtube.com/watch?v=${vid}`);
-
-            //Closing the input stream; imp, else it waits 
-            ytdl.stdin.end();
-            
-
-        } catch (error) {
-            next(err);
-    }
-});/*
+  
 
 /* Send mp4 download for video_id youtube */
 router.get('/download/video/:vid/:name?', (req,res,next)=>{
@@ -129,16 +80,3 @@ router.get('/download/video/:vid/:name?', (req,res,next)=>{
 
 module.exports = router;
 
-/**
- * 19
-
-Ffmpeg outputs all of its logging data to stderr, 
-to leave stdout free for piping the output data to some other program 
-or another ffmpeg instance.
-
-When running ffmpeg as an automatic process it's often useful give the option
-
--loglevel error
-which turns it completely mute in a normal scenario and only outputs 
-the error data (to stderr), which is normally what you would expect from a command-line program.
- */
