4cde58f1abd48962c9ca2550ddcbe0373f1721b2
Bijit Mondal <68532969+Bijit-Mondal@users.noreply.github.com>
2022-03-19 09:04:39 +0530

    Update youtube_search.js

--- routes/youtube_search.js
+++ routes/youtube_search.js
@@ -74,8 +74,47 @@ router.get('/download/audio/:vid/:name?', (req,res,next)=>{
 /* Send mp4 download for video_id youtube */
 router.get('/download/video/:vid/:name?', (req,res,next)=>{
   let {vid,name} = req.params;
-        vid = decodeURIComponent(vid);//video id
-        res.redirect(`http://www.9xyoutube.com/watch?v=${vid}`);
+    vid = decodeURIComponent(vid);/*video id */
+    name = name? decodeURIComponent(name):vid;/* Name: Optional Parameter for sending `name`.mp3 */
+    
+    //youtube-dl -x --audio-format mp3 http://www.youtube.com/watch?v=8veo4Uf6CR8 -f bestaudio   
+
+    /*Perfect CODE  */
+    /*Perfect CODE  */
+    try {
+            /* response attachment for triggering download instead of stream */
+            res.attachment(`${name}.mp4`);
+
+            /*Downloading ,Converting mp4 youtube video using video_id  */
+            const ytdl = spawn('youtube-dl', [
+                '-o',//output
+                '-',//stdout
+                '--format=18',
+               // 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',//best mp4 extension , else best
+                //'--recode-video',//recode video 
+                //'mp4',//to mp4 if not mp4
+                '-a',//input stream
+                '-'//stdin
+            ])
+            .on('error',(err)=>next(err))
+            .on('exit',(code)=>console.log(`Ytdl exited with code ${code}`));
+            
+            /* Setting output pipe first so that we dont lose any bits */
+            ytdl.stdout.pipe(res).on('error',(err)=>next(err));
+
+            /*Catching error on stdin */
+            ytdl.stdin.on('error',(err)=>next(err));
+
+            /* Writing video url to stdin for youtube-dl */
+            ytdl.stdin.write(`http://www.youtube.com/watch?v=${vid}`)
+
+            /*Closing the input stream; imp, else it waits */
+            ytdl.stdin.end();
+            
+
+        } catch (error) {
+            next(error);
+    }
 });
 
 module.exports = router;
